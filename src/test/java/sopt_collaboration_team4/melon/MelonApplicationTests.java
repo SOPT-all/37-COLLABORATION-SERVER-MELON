f23ef46a4155d55a3f19d643c03d96e9662043ec
dongchan0105 <come753zz@gmail.com>
2025-11-15 16:24:05 +0900

    Initial commit

---
+++ src/test/java/sopt_collaboration_team4/melon/MelonApplicationTests.java
@@ -0,0 +1,13 @@
+package sopt_collaboration_team4.melon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MelonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
