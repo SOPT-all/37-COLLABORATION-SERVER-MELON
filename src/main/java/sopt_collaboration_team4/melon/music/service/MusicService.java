48175a3291e16ba65837e346f138411309d8f612
dongchan0105 <come753zz@gmail.com>
2025-11-16 15:02:08 +0900

    [feat] MusicCount를 기준으로 top 8 을 가져오는 API와 하위 SERVICE로직 완성

--- src/main/java/sopt_collaboration_team4/melon/music/service/MusicService.java
+++ src/main/java/sopt_collaboration_team4/melon/music/service/MusicService.java
@@ -2,9 +2,25 @@ package sopt_collaboration_team4.melon.music.service;
 
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import sopt_collaboration_team4.melon.music.Music;
+import sopt_collaboration_team4.melon.music.repository.MusicRepository;
 
 @Service
 @RequiredArgsConstructor
 public class MusicService {
+
+    private final MusicRepository musicRepository;
+
+    public Page<Music> getPopularMusics(Pageable pageable) {
+        return musicRepository.findAllByOrderByPlayCountDesc(pageable);
+    }
+
+    //아직 요청사항에는 없음
+    public void increasePlayCount(long id) {
+        musicRepository.findById(id).ifPresent(Music::increasePlayCount);
+    }
+
 }
