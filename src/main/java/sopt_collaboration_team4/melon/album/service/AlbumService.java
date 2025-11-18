4d72bf57b5359b7b2457e3445f27012fafd621d3
dongchan0105 <come753zz@gmail.com>
2025-11-18 21:45:20 +0900

    [refactor]: ApiException으로 예외처리를 하여 나중에 공통예외처리를 쉽도록 변경했습니다.

--- src/main/java/sopt_collaboration_team4/melon/album/service/AlbumService.java
+++ src/main/java/sopt_collaboration_team4/melon/album/service/AlbumService.java
@@ -5,6 +5,9 @@ import org.springframework.stereotype.Service;
 import sopt_collaboration_team4.melon.album.Album;
 import sopt_collaboration_team4.melon.album.dto.AlbumResponse;
 import sopt_collaboration_team4.melon.album.repository.AlbumRepository;
+import sopt_collaboration_team4.melon.global.exception.ApiException;
+
+import static sopt_collaboration_team4.melon.global.exception.ErrorCode.ALBUM_NOT_FOUND;
 
 @Service
 @RequiredArgsConstructor
@@ -13,7 +16,7 @@ public class AlbumService {
     private final AlbumRepository albumRepository;
 
     public AlbumResponse getAlbumById(Long id) {
-        Album album = albumRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("없엉"));
+        Album album = albumRepository.findById(id).orElseThrow(() -> new ApiException(ALBUM_NOT_FOUND));
         return AlbumResponse.from(album);
     }
 }
